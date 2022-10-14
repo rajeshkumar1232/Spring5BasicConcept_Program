8183cbe3820a003b5329c71cfa57abd7347a5cdf
Rajesh kumar <47448471+rajeshkumar1232@users.noreply.github.com>
2022-10-14 15:05:33 +0530

    Add files via upload

---
+++ Spring5MethodReplacement/src/com/java/beans/MethodReplacerImpl.java
@@ -0,0 +1,17 @@
+package com.java.beans;
+
+import java.lang.reflect.Method;
+
+import org.springframework.beans.factory.support.MethodReplacer;
+
+public class MethodReplacerImpl implements MethodReplacer{
+
+	@Override
+	public Object reimplement(Object arg0, Method arg1, Object[] arg2) throws Throwable {
+		System.out.println("Course id       : C-222");
+		System.out.println("Course name     : Python");
+		System.out.println("Course cost     : 20000");
+		return null;
+	}
+
+}
---
+++ Spring5MethodReplacement/src/com/java/test/Test.java
@@ -0,0 +1,16 @@
+package com.java.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.java.beans.Course;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("/com/java/resources/applicationContext.xml");
+		Course course = (Course)context.getBean("course");
+		course.getCourseDetails();
+	}
+
+}
